806a7585065f697d90386c8f66a9f08c89ede37c
wjdgus1122 <qodqodantk@gmail.com>
2023-08-02 19:25:51 +0900

    mongoose code edit

--- main.js
+++ main.js
@@ -1,55 +1,78 @@
 const layouts = require("express-ejs-layouts");
-const MongoDB = require("mongodb").MongoClient,
-  dbURL = "mongodb:// localhost:27017",
-  dbName = "recipe_db";
-
-MongoDB.connect(dbURL, (error, client) => {
-  if (error) throw error;
-  let db = client.db(dbName);
-  db.collection("contacts")
-    .find()
-    .toArray((error, data) => {
-      if (error) throw err;
-      console.log(data);
-    });
+const mongoose = require("mongoose");
+var Subscriber = require("./models/subscriber");
+mongoose.connect("mongodb://localhost:27017/recipe_db", {
+  useNewUrlParser: true,
 });
-const express = require("express"),
-  app = express();
-
-app.set("port", process.env.PORT || 3000);
-app.set("view engine", "ejs");
-
-app.get("/items/:vegetable", homeController.sendReqParam);
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Confetti Cuisine!");
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
-app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSignUp);
-app.get("/thanks", homeController.postedContactForm);
+var subscriber1 = new Subscriber({
+  name: "Jon Wexler",
+  email: "jon@jonwaxler.com",
+});
 
-app.use(layouts);
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
+subscriber1.save((error, saveDocument) => {
+  if (error) console.log(error);
+  console.log(saveDocument);
+});
+
+Subscriber.create(
+  {
+    name: "Jon Wexler",
+    email: "jon@jonwexler.com",
+  },
+  function (error, savedDocument) {
+    if (error) console.log(error);
+    console.log(savedDocument);
+  }
 );
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-app.use(express.json());
-app.use(errorController.respondNoResourceFound);
-app.use(errorController.respondInternalError);
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  console.log(req.query);
-  res.send("POST Successful!");
+var myQuery = Subscriber.findOne({
+  name: "JH Kang",
+}).where("email", /Kang/);
+myQuery.exec((error, data) => {
+  if (data) console.log(data.name);
 });
+// const express = require("express"),
+//   app = express();
 
-app.listen(app.get("port"), () => {
-  console.log(`Server running at http://localhost:${app.get("port")}`);
-});
+// app.set("port", process.env.PORT || 3000);
+// app.set("view engine", "ejs");
+
+// app.get("/items/:vegetable", homeController.sendReqParam);
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to Confetti Cuisine!");
+// });
+
+// app.get("/courses", homeController.showCourses);
+// app.get("/contact", homeController.showSignUp);
+// app.get("/thanks", homeController.postedContactForm);
+
+// app.use(layouts);
+// app.use(
+//   express.urlencoded({
+//     extended: false,
+//   })
+// );
+// app.use(
+//   express.urlencoded({
+//     extended: false,
+//   })
+// );
+// app.use(express.json());
+// app.use(errorController.respondNoResourceFound);
+// app.use(errorController.respondInternalError);
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+//   console.log(req.query);
+//   res.send("POST Successful!");
+// });
+
+// app.listen(app.get("port"), () => {
+//   console.log(`Server running at http://localhost:${app.get("port")}`);
+// });
---
+++ models/subscriber.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const subscriberSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  zipCode: Number,
+});
+module.exports = mongoose.model("Subscriber", subscriberSchema);
