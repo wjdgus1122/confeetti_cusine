86af0a7d1ac36a71ed5e88daf1f58fcf799063e7
wjdgus1122 <qodqodantk@gmail.com>
2023-08-31 20:51:45 +0900

    chat msg save code edit

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,3 +1,5 @@
+const Message = require("../models/message");
+
 module.exports = (io) => {
   io.on("connection", (client) => {
     console.log("new connection");
@@ -6,10 +8,25 @@ module.exports = (io) => {
       console.log("user disconnected");
     });
 
-    client.on("message", () => {
-      io.emit("message", {
-        content: "Hello",
-      });
+    client.on("message", (data) => {
+      let messageAttributes = {
+        content: data.content,
+        userName: data.userName,
+        user: data.userId,
+      };
+      m = new Message(messageAttributes);
+      m.save()
+        .then(() => {
+          io.emit("message", messageAttributes);
+        })
+        .catch((error) => console.log(`error : ${error.message}`));
     });
   });
+
+  Message.find({})
+    .sort({ createAt: -1 })
+    .limit(10)
+    .then((message) => {
+      client.emit("load all messages", message.reverse());
+    });
 };
--- public/js/recipeApp.js
+++ public/js/recipeApp.js
@@ -31,16 +31,36 @@ $(document).ready(() => {
   const socket = io();
 
   $("#chatForm").submit(() => {
-    socket.emit("message");
+    let text = $("#chat_input").val(),
+      userName = $("#chat-user-name").val(),
+      userId = $("#chat-user-id").val();
+    socket.emit("message", {
+      content: text,
+      userName: userName,
+      userId: userId,
+    });
     $("#chat-input").val("");
     return false;
   });
-  socket.on("message", (message) => {
-    displayMessage(message.content);
+  socket.on("load all message", (data) => {
+    data.forEach((message) => {
+      displayMessage(message);
+    });
   });
 
   let displayMessage = (message) => {
-    $("#chat").prepend($("<li>").html(message));
+    $("#chat").prepend(
+      $("<li>").html(`
+      <strong class="message ${getCurrentUserClass(message.user)}">
+        ${message.userName}
+      </strong> : ${message.content}
+    `)
+    );
+  };
+
+  let getCurrentUserClass = (id) => {
+    let userId = $("#chat-user-id").val();
+    return userId === id ? "current-user" : "";
   };
 });
 
